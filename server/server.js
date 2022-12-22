df14d33966f8d46de809a4ea604273fbde7200d9
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-12-22 17:00:06 +0530

    removed: message stuff

--- server/server.js
+++ server/server.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const { Server } = require('socket.io');
-const http = require('http');
-
 require('express-async-errors');
 dotenv.config();
 
@@ -19,20 +16,12 @@ mongoConnect();
 
 const app = express();
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
-});
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(errorHandler);
 
 app.use('/api/auth', require('./routes/authRoute'));
-app.use('/api/message', require('./routes/messageRoute'));
 
 mongoose.connection.once('open', () => {
   console.log('Connected to database');

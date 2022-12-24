94767c5ab5625c6e471df36fd37bec69d489d3da
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-12-24 06:20:03 +0530

    feature: grp chat & add users to group chat

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -59,3 +59,61 @@ exports.getChat = async (req, res) => {
 
   res.status(200).json(foundChat);
 };
+
+exports.createGroupChat = async (req, res) => {
+  const { chatName, users } = req.body;
+
+  if (!chatName || !users)
+    return res.status(400).json({ error: 'Chat name and users are required' });
+
+  if (users.length < 2)
+    return res.status(400).json({ error: 'Add at least two users' });
+
+  users.push(req.userId);
+
+  const newChat = await Chat.create({
+    chatName,
+    users,
+    isGroup: true,
+  });
+
+  const populatedChat = await Chat.findOne({ _id: newChat._id }).populate(
+    'users',
+    '-password'
+  );
+
+  res.status(200).json(populatedChat);
+};
+
+exports.addUserGroupChat = async (req, res) => {
+  const { chatId, users } = req.body;
+
+  if (!chatId || !users)
+    return res.status(400).json({ error: 'Chat id and users are required' });
+
+  const foundChat = await Chat.findOne({ _id: chatId });
+
+  if (!foundChat) return res.status(400).json({ error: 'Chat not found' });
+
+  if (!foundChat.isGroup)
+    return res.status(400).json({ error: 'Chat is not a group' });
+
+  const usersInChat = users.filter((user) => {
+    return foundChat.users.includes(user);
+  });
+
+  if (usersInChat.length > 0)
+    return res.status(201).json({ message: 'Users are already in the chat' });
+
+  const newUsers = [...foundChat.users, ...users];
+
+  const updatedChat = await Chat.findOneAndUpdate(
+    { _id: chatId },
+    { users: newUsers },
+    { new: true }
+  ).populate('users', '-password');
+
+  res.status(200).json(updatedChat);
+};
+
+
--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const verifyHandler = require('../middleware/verifyHandler');
-const { createChat, getChat } = require('../controller/chatController');
+const {
+  createChat,
+  getChat,
+  createGroupChat,
+  addUserGroupChat,
+} = require('../controller/chatController');
 
 router.use((req, res, next) => {
   console.log('Chat Route');
@@ -8,5 +13,9 @@ router.use((req, res, next) => {
 });
 
 router.route('/').post(verifyHandler, createChat).get(verifyHandler, getChat);
+
+router.route('/group').post(verifyHandler, createGroupChat);
+
+router.route('/group/add').post(verifyHandler, addUserGroupChat);
 
 module.exports = router;

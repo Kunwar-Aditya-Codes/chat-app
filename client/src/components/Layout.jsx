f1a58f42f4c58b3c74ffca04f995b898860712ca
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-12-25 07:32:45 +0530

    added: footer

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,8 +2,16 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className=' h-screen bg-gradient-to-br from-[#040814] via-[#030a1a] to-[#050514] text-[#d8d8d8]'>
-      <Outlet />
+    <div className='flex h-screen flex-col bg-gradient-to-br from-[#040814] via-[#030a1a] to-[#050514] text-[#d8d8d8]'>
+      <div className='flex-grow'>
+        <Outlet />
+      </div>
+      <footer className='bg-sky-900/10 p-4'>
+        <p className='text-center text-sm'>
+          &copy; {new Date().getFullYear()} Chat App - Kunwar Aditya. All rights
+          reserved.
+        </p>
+      </footer>
     </div>
   );
 };

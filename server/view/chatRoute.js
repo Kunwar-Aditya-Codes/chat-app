3e71751b41b7f359fd2cf006e26086d6029d039e
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-01 13:02:51 +0530

    updated: added verify token to all private routes

--- server/view/chatRoute.js
+++ server/view/chatRoute.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const { createChat, getChat } = require('../controller/chatController');
+const tokenVerifyHandler = require('../middleware/tokenVerifyHandler');
 
 router.use((req, res, next) => {
   console.log('Chat route');
   next();
 });
+
+router.use(tokenVerifyHandler);
 
 router.post('/', createChat);
 router.get('/:userid', getChat);
--- server/view/messageRoute.js
+++ server/view/messageRoute.js
@@ -3,11 +3,14 @@ const {
   createMessage,
   getMessages,
 } = require('../controller/messageController');
+const tokenVerifyHandler = require('../middleware/tokenVerifyHandler');
 
 router.use((req, res, next) => {
   console.log('Message route');
   next();
 });
+
+router.use(tokenVerifyHandler);
 
 router.post('/', createMessage);
 router.get('/:chatId', getMessages);

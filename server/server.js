94845b8b14fdae5d6f04cb019f754e26a8df42ba
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-01 09:04:56 +0530

    feature: error handler

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+require('express-async-errors');
 require('dotenv').config();
 
+// External modules
 const connectDb = require('./utils/mongoConnect');
+const errorHandler = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-connectDb();
+connectDb(); // Connect to db before starting the server
+
+app.use(express.json()); // Parse JSON bodies
+app.use(cors()); // Enable CORS
+app.use(errorHandler); // Error handler
 
 mongoose.connection.once('open', () => {
   console.log('Connected to db!');

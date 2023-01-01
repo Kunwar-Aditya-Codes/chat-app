52623ab81963e5aa6445b6c677ddec22ee60fd43
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-01 12:55:25 +0530

    feature: specific user info

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,7 +3,24 @@ const User = require('../model/User');
 // @route   GET api/user/:userId
 // @desc    Get a user
 // @access  Private
-exports.getUser = async (req, res) => {};
+exports.getUser = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    return res.status(400).json({ message: 'Missing fields' });
+  }
+
+  const foundUser = await User.findById(userId)
+    .select('-password')
+    .lean()
+    .exec();
+
+  if (!foundUser) {
+    return res.status(203).json({ message: 'No user found' });
+  }
+
+  res.status(200).json(foundUser);
+};
 
 // @route   GET api/user
 // @desc    Get logged in user

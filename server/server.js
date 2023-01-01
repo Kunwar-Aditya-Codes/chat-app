4298ff889e0787b5d286a1f2f816b1846cda01e2
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-01 08:43:01 +0530

    feature: connected database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+const connectDb = require('./utils/mongoConnect');
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Server up and running');
-});
+connectDb();
 
-app.listen('3000', function () {
-  console.log('Server Started !!');
+mongoose.connection.once('open', () => {
+  console.log('Connected to db!');
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT:${PORT}`);
+  });
 });

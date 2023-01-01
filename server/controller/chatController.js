dc1f05b27d9ac80a22a213bc6108533a08e01ce2
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-01 10:57:21 +0530

    feature: create chat

--- server/controller/chatController.js
+++ server/controller/chatController.js
@@ -4,7 +4,39 @@ const User = require('../model/User');
 // @route   POST api/chat
 // @desc    Create a chat
 // @access  Public
-exports.createChat = async (req, res) => {};
+exports.createChat = async (req, res) => {
+  const { senderId, receiverId } = req.body;
+
+  if (!senderId || !receiverId) {
+    return res.status(400).json({ msg: 'Missing fields' });
+  }
+
+  const foundChat = await Chat.findOne({
+    users: { $all: [senderId, receiverId] },
+  });
+
+  if (foundChat) {
+    const populatedChat = await Chat.findById(foundChat._id).populate(
+      'users',
+      'username email profilePicture'
+    );
+
+    return res.status(200).json(populatedChat);
+  }
+
+  const newChat = new Chat({
+    users: [senderId, receiverId],
+  });
+
+  const savedChat = await newChat.save();
+
+  const populatedChat = await Chat.findById(savedChat._id).populate(
+    'users',
+    'username email profilePicture'
+  );
+
+  res.status(200).json(populatedChat);
+};
 
 // @route   GET api/chat/:chatid
 // @desc    Get all chats

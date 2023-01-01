835c6cab678de4a2ddd4975723b20687adb22090
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-01 09:25:10 +0530

    feature: register user

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,9 +1,39 @@
 const User = require('../model/User');
+const bcrypt = require('bcrypt');
 
 // @route   POST api/auth/register
 // @desc    Register a user
 // @access  Public
-exports.register = async (req, res) => {};
+exports.register = async (req, res) => {
+  const { username, email, password, profilePic } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      message: 'Please enter all fields',
+    });
+  }
+
+  const foundUser = await User.findOne({ email }).lean().exec();
+
+  if (foundUser) {
+    return res.status(400).json({
+      message: 'User already exists',
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+    profilePic,
+  });
+
+  return res.status(201).json({
+    message: 'User created successfully',
+  });
+};
 
 // @route   POST api/auth/login
 // @desc    Login a user

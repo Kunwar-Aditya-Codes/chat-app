50d7f859c3db0ca08c2b6c8bd13d7a5b86a1ed57
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-02 11:22:46 +0530

    added: Register page

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className='h-screen bg-zinc-900 text-zinc-300'>
+    <div className='h-screen bg-[#060809] text-zinc-300'>
       <Outlet />
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,60 @@
+import { Link } from 'react-router-dom';
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className='h-full flex items-center justify-center'>
+      <div className='h-[80%] w-[45%] flex items-center justify-center bg-gradient-to-br from-indigo-600 to-sky-600 rounded-md'>
+        <div className='h-[99.5%] w-[99.5%] bg-black/90 rounded-md'>
+          <form className='flex flex-col mx-24 justify-evenly text-lg p-4 h-full'>
+            <input
+              type='text'
+              name='username'
+              placeholder='Username'
+              className='outline-none bg-transparent border-2 border-indigo-600 rounded-md py-3 px-2'
+            />
+
+            <input
+              type='email'
+              name='email'
+              placeholder='Email'
+              className='outline-none bg-transparent border-2 border-indigo-600 rounded-md py-3 px-2'
+            />
+
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              className='outline-none bg-transparent border-2 border-indigo-600 rounded-md py-3 px-2'
+            />
+
+            <input
+              type='file'
+              name='profilePic'
+              className='outline-none bg-transparent border-2 border-indigo-600 rounded-md 
+                cursor-pointer file:cursor-pointer file:bg-indigo-600  file:text-white   file:mr-2  file:rounded-md file:py-2 file:px-3 
+              '
+            />
+
+            <button
+              type='submit'
+              className='outline-none bg-indigo-600 rounded-md py-3 px-2 disabled:bg-gray-500 disabled:cursor-not-allowed'
+            >
+              Register
+            </button>
+
+            <p className='text-center '>
+              Already have an account?
+              <Link
+                to='/login'
+                className='text-indigo-400 underline underline-offset-2 ml-2'
+              >
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Register;

cb54e4c67dc367889773e91f84a75b5c9069ab38
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-02 10:53:02 +0530

    added: register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Register from './pages/Register';
 import Welcome from './pages/Welcome';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route index path='/' element={<Welcome />} />
+        <Route path='register' element={<Register />} />
       </Route>
     </Routes>
   );

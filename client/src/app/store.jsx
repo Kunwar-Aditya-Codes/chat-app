41868358010f7e902762eb9e1b31ef3bb3b1c83d
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-02 11:30:41 +0530

    added: redux store

---
+++ client/src/app/store.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: false,
+});
+
+setupListeners(store.dispatch);

cf225d4d8f1052de0128cd5da616b1fee0c916a6
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-01-07 11:00:55 +0530

    url change

--- client/src/app/apiSlice.jsx
+++ client/src/app/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from './slices/authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000/api',
+  baseUrl: 'https://mern-chat-w5is.onrender.com/api',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState()?.auth?.token;
--- server/utils/corsConfig.js
+++ server/utils/corsConfig.js
@@ -1,4 +1,8 @@
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3001'];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3001',
+  'https://mern-chat-w5is.onrender.com',
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
